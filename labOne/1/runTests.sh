b4cbb4f8ffc793f25c4bf4f8419a7665a833671e
BartLunenborg <b.lunenborg@student.rug.nl>
2023-11-18 19:52:27 +0100

    Passed 10/10

---
+++ labOne/1/runTests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+EXECUTABLE="./main"
+TEST_DIR="./tests"
+
+totalTests=0
+passedTests=0
+failedTests=""
+
+# Run tests
+for infile in "$TEST_DIR"/*.in; do
+    totalTests=$((totalTests + 1))
+
+    expected="${infile%.in}.out"
+    actual=$($EXECUTABLE < "$infile")
+
+    if [ "$actual" = "$(cat $expected)" ]; then
+        passedTests=$((passedTests + 1))   
+    else 
+        failedTests+="In test $infile:\nexpected: $(cat $expected)\nactual  : $actual"
+    fi
+done
+
+# Print test results
+if [ "$totalTests" -eq "$passedTests" ]; then
+    echo "Passed all $totalTests/$totalTests tests!"
+else
+    echo -e "There were test failures, passed $passedTests/$totalTests tests:\n$failedTests"
+fi
