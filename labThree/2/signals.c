3225bc829de9a427a04050ac4872ba219d6cfbd2
BartLunenborg <b.lunenborg@student.rug.nl>
2024-01-01 18:42:25 +0100

    Passed 10/10

---
+++ labThree/2/signals.c
@@ -0,0 +1,165 @@
+/** @file   signals.c
+ *  @brief  Implementations of functions in signals.h
+ *  @author Bart Lunenborg, s3410579
+ */
+
+#include "signals.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <complex.h>
+
+typedef _Complex double cplx;
+double PI = 3.14159265358979323846;
+
+Signal readSignal() {
+  int len, temp;
+  cplx *arr;
+  char c;
+  scanf("%d:", &len);
+  arr = calloc(len, sizeof(cplx));
+  do c = getchar(); while (c != '[');
+  if (len > 0) {
+    scanf("%d", &temp);
+    arr[0] = temp;
+    for (int i=1; i < len; i++) {
+      scanf(",%d", &temp);
+      arr[i] = temp;
+    }
+  }
+  do c = getchar(); while (c != ']');
+  Signal s = {len, arr};
+  return s;
+}
+
+void printSignal(const Signal s) {
+  printf("%d: [", s.length);
+  if (s.length > 0) {
+    printf("%d", (int)round(creal(s.signal[0])));
+    for (int i=1; i < s.length; i++) {
+      printf(",%d", (int)round(creal(s.signal[i])));
+    }
+  }
+  printf("]\n");
+}
+
+void freeSignals(Signal *ss, const int length) {
+  for (int i = 0; i < length; i++) {
+    free(ss[i].signal);
+  }
+  free(ss);
+}
+
+// finds the smallest number > n that is a power of two
+int powerOfTwo(int n) {
+  int p2 = 1;
+  while (p2 < n) {
+    p2 *= 2;
+  }
+  return p2;
+}
+
+// splits the even and odd indices into two different arrays
+void splitEvenOdd(const Signal a, Signal *signals) {
+  int n = a.length;
+  Signal aEven = {n/2, calloc(n/2, sizeof(cplx))};
+  Signal aOdd  = {n/2, calloc(n/2, sizeof(cplx))};
+  for (int i = 0; i < n; i += 2) {
+    aEven.signal[i/2] = a.signal[i];
+    aOdd.signal[i/2] = a.signal[i+1];
+  }
+  signals[0] = aEven;
+  signals[1] = aOdd;
+}
+
+// Cooley and Tukey fft algorithm
+Signal fft(const Signal a, const cplx omega) {
+  int n = a.length;
+  if (n == 1) {
+    Signal y = {n, calloc(n, sizeof(cplx))};
+    y.signal[0] = a.signal[0];
+    return y;
+  }
+  cplx x = 1;
+  Signal *signals = calloc(4, sizeof(Signal));
+  splitEvenOdd(a, signals);  // signals[0] = a_even, signals[1] = a_odd
+  signals[2] = fft(signals[0], omega * omega);  // y_even
+  signals[3] = fft(signals[1], omega * omega);  // y_odd
+  Signal y = {n, calloc(n, sizeof(cplx))};
+  for (int i = 0; i < n/2; i++) {
+    y.signal[i]     = signals[2].signal[i] + x * signals[3].signal[i];
+    y.signal[i+n/2] = signals[2].signal[i] - x * signals[3].signal[i];
+    x *= omega;
+  }
+  freeSignals(signals, 4);
+  return y; 
+}
+
+Signal *padZeros(const Signal a, const Signal b, int n) {
+  Signal *signals = calloc(2, sizeof(Signal));
+  Signal xMod = {n, calloc(n, sizeof(cplx))};
+  Signal hMod = {n, calloc(n, sizeof(cplx))};
+  for (int i = 0; i < a.length; i++) {
+    xMod.signal[i] = a.signal[i];
+  }
+  for (int i = 0; i < b.length; i++) {
+    hMod.signal[i] = b.signal[i];
+  }
+  signals[0] = xMod;
+  signals[1] = hMod;
+  return signals;
+}
+
+Signal *ffts(Signal *signals, cplx omega) {
+  Signal *ffts = calloc(2, sizeof(Signal));
+  ffts[0] = fft(signals[0], omega);
+  ffts[1] = fft(signals[1], omega);
+  freeSignals(signals, 2);
+  return ffts;
+}
+
+Signal convolve(const Signal x, const Signal h) {
+  int length = x.length + h.length - 1;
+  int n = powerOfTwo(length);
+
+  // fft of each (after padding x and h with zeros)
+  cplx omega = cexp(2 * I * PI / n);
+  Signal *fftxh = ffts(padZeros(x, h, n), omega);
+
+  // component multiply
+  for (int i = 0; i < n; i++) {
+    fftxh[0].signal[i] *= fftxh[1].signal[i];
+  }
+
+  // inverse fft
+  omega = conj(omega);
+  Signal y = fft(fftxh[0], omega);
+  freeSignals(fftxh, 2);
+  // adjust y
+  y.length = length;
+  for (int i = 0; i < length; i++) {
+    y.signal[i] /= n;
+  }
+  return y;
+}
+
+Signal reverse(const Signal s) {
+  Signal reverse = {s.length, calloc(s.length, sizeof(Signal))};
+  for (int i = 0; i < s.length; i++) {
+    reverse.signal[i] = s.signal[s.length - 1 - i];
+  }
+  return reverse;
+}
+
+Signal correlate(const Signal x, const Signal h) {
+  Signal *signals = calloc(2, sizeof(Signal));
+  signals[0] = reverse(h);
+  signals[1] = convolve(x, signals[0]);
+  int length = x.length - h.length + 1;
+  Signal y = {length, calloc(length, sizeof(cplx))};
+  for (int i = 0; i < length; i++) {
+    y.signal[i] = signals[1].signal[i + h.length - 1];
+  }
+  freeSignals(signals, 2);
+  return y;
+}
---
+++ labThree/2/signals.h
@@ -0,0 +1,58 @@
+/** @file   signals.h
+ *  @brief  Functions for signal operations
+ *  @author Bart Lunenborg, s3410579
+ */
+
+#ifndef __SIGNALS_H__
+#define __SIGNALS_H__
+
+/**
+ * A discrete Signal contain length complex doubles.
+ */
+typedef struct Signal {
+  int  length;              /**< The length of the Signal  */
+  _Complex double *signal;  /**< The array with the Signal */
+} Signal;
+
+
+/**
+ * Given the length of a discrete Signal, reads it from the input.
+ * @return  The read Signal (to be freed by caller)
+ */
+Signal readSignal();
+
+
+/**
+ * Given a pointer to an array of integers and its length, prints it formatted [1,2,3..n].
+ * @param signal  The Signal to print
+ */
+void printSignal(const Signal signal);
+
+
+/**
+ * Given an array of Signals, frees them.
+ * @param signals  The Signals array to be freed
+ * @param length   The length of the array
+ */
+void freeSignals(Signal *signals, const int length);
+
+
+/** 
+ * Returns the convolution of Signal x and h using the fft.
+ * @param x  The input Signal
+ * @param h  The filter
+ * @return   The resulting Signal after convolution (to be freed by caller )
+ */
+Signal convolve(const Signal x, const Signal h);
+
+
+/**
+ * Given Signal x and h finds the steady state of y.
+ * Obtained by correlating x with h using the fft.
+ * @Param x  The input Signal
+ * @Param h  The template Signal (h.length should be < x.length)
+ * @return   The Signal obtained by correlating x with h (to be freed by caller)
+ */
+Signal correlate(const Signal x, const Signal h);
+
+#endif
