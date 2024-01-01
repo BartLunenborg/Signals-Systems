dd86e060f41375003a47efd7114e7a731f2ada1d
BartLunenborg <b.lunenborg@student.rug.nl>
2024-01-01 17:46:56 +0100

    Passed 10/10

--- labThree/1/signals.c
+++ labThree/1/signals.c
@@ -3,21 +3,29 @@
  *  @author Bart Lunenborg, s3410579
  */
 
+#include "signals.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <complex.h>
 #include <math.h>
-#include "signals.h"
+#include <complex.h>
+
+typedef _Complex double cplx;
+double PI = 3.14159265358979323846;
 
 Signal readSignal() {
-  int len, *arr;
+  int len, temp;
+  cplx *arr;
   char c;
   scanf("%d:", &len);
-  arr = calloc(len, sizeof(int));
+  arr = calloc(len, sizeof(cplx));
   do c = getchar(); while (c != '[');
   if (len > 0) {
-    scanf("%d", &arr[0]);
-    for (int i=1; i < len; i++) scanf(",%d", &arr[i]);
+    scanf("%d", &temp);
+    arr[0] = temp;
+    for (int i=1; i < len; i++) {
+      scanf(",%d", &temp);
+      arr[i] = temp;
+    }
   }
   do c = getchar(); while (c != ']');
   Signal s = {len, arr};
@@ -27,101 +35,110 @@ Signal readSignal() {
 void printSignal(const Signal s) {
   printf("%d: [", s.length);
   if (s.length > 0) {
-    printf("%d", s.signal[0]);
-    for (int i=1; i < s.length; i++) printf(",%d", s.signal[i]);
+    printf("%d", (int)round(creal(s.signal[0])));
+    for (int i=1; i < s.length; i++) {
+      printf(",%d", (int)round(creal(s.signal[i])));
+    }
   }
   printf("]\n");
-}
-
-void printPearson(const Pearson p) {
-  printf("%d: [", p.length);
-  if (p.length > 0) {
-    printf("%.5lf", p.corrs[0]);
-    for (int i=1; i < p.length; i++) printf(",%.5lf", p.corrs[i] < 0 && p.corrs[i] > -0.00001 ? 0.00000 : p.corrs[i]);
-  }
-  printf("]\n");
-}
-
-void freeSignal(Signal s) {
-  free(s.signal);
 }
 
 void freeSignals(Signal *ss, const int length) {
   for (int i = 0; i < length; i++) {
-    freeSignal(ss[i]);
+    free(ss[i].signal);
   }
   free(ss);
 }
 
+// finds the smallest number > n that is a power of two
+int powerOfTwo(int n) {
+  int p2 = 1;
+  while (p2 < n) {
+    p2 *= 2;
+  }
+  return p2;
+}
+
+// splits the even and odd indices into two different arrays
+void splitEvenOdd(const Signal a, Signal *signals) {
+  int n = a.length;
+  Signal aEven = {n/2, calloc(n/2, sizeof(cplx))};
+  Signal aOdd  = {n/2, calloc(n/2, sizeof(cplx))};
+  for (int i = 0; i < n; i += 2) {
+    aEven.signal[i/2] = a.signal[i];
+    aOdd.signal[i/2] = a.signal[i+1];
+  }
+  signals[0] = aEven;
+  signals[1] = aOdd;
+}
+
+// Cooley and Tukey fft algorithm
+Signal fft(const Signal a, const cplx omega) {
+  int n = a.length;
+  if (n == 1) {
+    Signal y = {n, calloc(n, sizeof(cplx))};
+    y.signal[0] = a.signal[0];
+    return y;
+  }
+  cplx x = 1;
+  Signal *signals = calloc(4, sizeof(Signal));
+  splitEvenOdd(a, signals);  // signals[0] = a_even, signals[1] = a_odd
+  signals[2] = fft(signals[0], omega * omega);  // y_even
+  signals[3] = fft(signals[1], omega * omega);  // y_odd
+  Signal y = {n, calloc(n, sizeof(cplx))};
+  for (int i = 0; i < n/2; i++) {
+    y.signal[i]     = signals[2].signal[i] + x * signals[3].signal[i];
+    y.signal[i+n/2] = signals[2].signal[i] - x * signals[3].signal[i];
+    x *= omega;
+  }
+  freeSignals(signals, 4);
+  return y; 
+}
+
+Signal *padZeros(const Signal a, const Signal b, int n) {
+  Signal *signals = calloc(2, sizeof(Signal));
+  Signal xMod = {n, calloc(n, sizeof(cplx))};
+  Signal hMod = {n, calloc(n, sizeof(cplx))};
+  for (int i = 0; i < a.length; i++) {
+    xMod.signal[i] = a.signal[i];
+  }
+  for (int i = 0; i < b.length; i++) {
+    hMod.signal[i] = b.signal[i];
+  }
+  signals[0] = xMod;
+  signals[1] = hMod;
+  return signals;
+}
+
+Signal *ffts(Signal *signals, cplx omega) {
+  Signal *ffts = calloc(2, sizeof(Signal));
+  ffts[0] = fft(signals[0], omega);
+  ffts[1] = fft(signals[1], omega);
+  freeSignals(signals, 2);
+  return ffts;
+}
+
 Signal convolve(const Signal x, const Signal h) {
   int length = x.length + h.length - 1;
-  Signal y = {length, calloc(length, sizeof(int))};
-  for (int i = 0; i < x.length; i++) {
-    for (int j = 0; j < h.length; j++) {
-      y.signal[i+j] += x.signal[i] * h.signal[j];
-    }
+  int n = powerOfTwo(length);
+
+  // fft of each (after padding x and h with zeros)
+  cplx omega = cexp(2 * I * PI / n);
+  Signal *fftxh = ffts(padZeros(x, h, n), omega);
+
+  // component multiply
+  for (int i = 0; i < n; i++) {
+    fftxh[0].signal[i] *= fftxh[1].signal[i];
+  }
+
+  // inverse fft
+  omega = conj(omega);
+  Signal y = fft(fftxh[0], omega);
+  freeSignals(fftxh, 2);
+  // adjust y
+  y.length = length;
+  for (int i = 0; i < length; i++) {
+    y.signal[i] /= n;
   }
   return y;
-}
-
-Signal firFilterH(const Signal x, const Signal y) {
-  int length = y.length - x.length + 1;
-  int *arr = calloc(length, sizeof(int));
-  int *sums = calloc(y.length, sizeof(int));
-  for (int i = 0; i < length; i++) {
-    arr[i] = (y.signal[i] - sums[i]) / x.signal[0];
-    for (int j = 0; j < x.length; j++) {
-      sums[i+j] += arr[i] * x.signal[j];
-    }
-  }
-  Signal h = {length, arr};
-
-  for (int i = length; i < y.length; i++) {
-    if (sums[i] != y.signal[i]) {
-      free(sums);
-      h.length = -1;
-      return h;
-    }
-  }
-
-  free(sums);
-  return h;
-}
-
-Signal correlate(const Signal x, const Signal h) {
-  int length = x.length - h.length + 1;
-  int *arr = calloc(length, sizeof(int));
-  for (int i = 0; i < length; i++) {
-    for (int j = 0; j < h.length; j++) {
-      arr[i] += h.signal[j] * x.signal[j + i];
-    }
-  }
-  Signal y = {length, arr};
-  return y;
-}
-
-Pearson pearsonCorrelate(const Signal x, const Signal h) {
-  double mean_h = 0;
-  for (int i = 0; i < h.length; i++) mean_h += h.signal[i];
-  mean_h /= h.length;
-  
-  int length = x.length - h.length + 1;
-  double *arr = malloc(length * sizeof(double));
-  for (int i = 0; i < length; i++) {
-    double mean_x = 0;
-    for (int j = 0; j < h.length; j++) mean_x += x.signal[j + i];
-    mean_x /= h.length;
-    double cov = 0;
-    double sd_x = 0;
-    double sd_h = 0;
-    for (int j = 0; j < h.length; j++) {
-      cov  += (h.signal[j] - mean_h) * (x.signal[j+i] - mean_x);
-      sd_x += (x.signal[j+i] - mean_x) * (x.signal[j+i] - mean_x);
-      sd_h += (h.signal[j] - mean_h) * (h.signal[j] - mean_h);
-    }
-    arr[i] = cov / (sqrt(sd_x) * sqrt(sd_h));
-  }
-
-  Pearson p = {length, arr};
-  return p;
 }
--- labThree/1/signals.h
+++ labThree/1/signals.h
@@ -7,21 +7,12 @@
 #define __SIGNALS_H__
 
 /**
- * A discrete Signal contain length integers.
+ * A discrete Signal contain length complex doubles.
  */
 typedef struct Signal {
-  int  length;  /**< The length of the Signal  */
-  int *signal;  /**< The array with the Signal */
+  int  length;              /**< The length of the Signal  */
+  _Complex double *signal;  /**< The array with the Signal */
 } Signal;
-
-
-/**
- * Pearson correlation values of a Signal.
- */
-typedef struct Pearson {
-  int    length;  /**< The length of the array */ 
-  double *corrs;  /**< The correlation values  */
-} Pearson;
 
 
 /**
@@ -39,21 +30,7 @@ void printSignal(const Signal signal);
 
 
 /**
- * Given an array of Pearson correlations, prints is (5 digits precision).
- * @param p  The Pearson array
- */
-void printPearson(const Pearson p);
-
-
-/**
- * Given a Signal, frees it.
- * @param signal  The Signal to be freed
- */
-void freeSignal(Signal signal);
-
-
-/**
- * Given an array of Signals, frees it.
+ * Given an array of Signals, frees them.
  * @param signals  The Signals array to be freed
  * @param length   The length of the array
  */
@@ -61,40 +38,11 @@ void freeSignals(Signal *signals, const int length);
 
 
 /** 
- * Returns the convolution of Signal x and h.
+ * Returns the convolution of Signal x and h using the fft.
  * @param x  The input Signal
  * @param h  The filter
  * @return   The resulting Signal after convolution (to be freed by caller )
  */
 Signal convolve(const Signal x, const Signal h);
-
-/**
- * Given Signal x and y, finds Signal h.
- * If h did not produce y from x, return h with length = -1.
- * Length = -1 indicate that the Signal y was not produced by x through a FIR filter.
- * @param x  The input Signal
- * @param y  The output Signal
- * @return   The Signal h that made y from x (to be freed by caller)
- */
-Signal firFilterH(const Signal x, const Signal y);
-
-
-/**
- * Given Signal x and h finds the steady state of y.
- * Obtained by correlating x with h.
- * @param x  The input Signal
- * @param h  The template Signal (h.length should be < x.length)
- * @return   The Signal obtained by correlating x with h (to be freed by caller)
- */
-Signal correlate(const Signal x, Signal h);
-
-
-/** 
- * Given Signal x and h, returns the Pearson correlation values
- * @param x  The input Signal
- * @param h  The template Signal (h.length should be < x.length)
- * @return   The Pearson correlation values (to be freed by caller)
- */
-Pearson pearsonCorrelate(const Signal x, const Signal h);
 
 #endif
