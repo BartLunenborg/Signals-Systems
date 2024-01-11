da75e4c080caaa723cc06e73825ea69e8c49ec10
BartLunenborg <b.lunenborg@student.rug.nl>
2024-01-11 16:49:06 +0100

    Passed 10/10

---
+++ labThree/6/main.c
@@ -0,0 +1,18 @@
+/** @file   main.c
+ *  @brief  Problem 6 of lab 3
+ *  @author Bart Lunenborg, s3410579
+ */
+
+#include "signals.h"
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  Signal *signals = malloc(3 * sizeof(Signal));
+  signals[0] = readSignal();
+
+  signals[1] = intt(signals[0]);
+  printSignal(signals[1]);
+
+  freeSignals(signals, 2);
+  return 0;
+}
---
+++ labThree/6/signals.c
@@ -0,0 +1,118 @@
+/** @file   signals.c
+ *  @brief  Implementations of functions in signals.h
+ *  @author Bart Lunenborg, s3410579
+ */
+
+#include "signals.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int uint;
+uint PRIME = 40961;
+uint LEN   = 8192;
+uint OMEGA = 243;
+
+Signal readSignal() {
+  uint len, temp, *arr;
+  char c;
+  scanf("%u:", &len);
+  arr = calloc(len, sizeof(uint));
+  do c = getchar(); while (c != '[');
+  if (len > 0) {
+    scanf("%u", &temp);
+    arr[0] = temp;
+    for (uint i=1; i < len; i++) {
+      scanf(",%u", &temp);
+      arr[i] = temp;
+    }
+  }
+  do c = getchar(); while (c != ']');
+  Signal s = {len, arr};
+  return s;
+}
+
+void printSignal(const Signal s) {
+  printf("%u: [", s.length);
+  if (s.length > 0) {
+    printf("%u", s.signal[0]);
+    for (int i=1; i < s.length; i++) {
+      printf(",%u", s.signal[i]);
+    }
+  }
+  printf("]\n");
+}
+
+void freeSignals(Signal *ss, const uint length) {
+  for (uint i = 0; i < length; i++) {
+    free(ss[i].signal);
+  }
+  free(ss);
+}
+
+Signal *_splitEvenOdd(const Signal s) {
+  Signal *signals = malloc(4 * sizeof(Signal));
+  int n = s.length;
+  Signal aEven = {n/2, calloc(n/2, sizeof(uint))};
+  Signal aOdd  = {n/2, calloc(n/2, sizeof(uint))};
+  for (int i = 0; i < n; i += 2) {
+    aEven.signal[i/2] = s.signal[i];
+    aOdd.signal[i/2] = s.signal[i+1];
+  }
+  signals[0] = aEven;
+  signals[1] = aOdd;
+  return signals;
+}
+
+Signal _ntt(const Signal s, const uint omega) {
+  uint n = s.length;
+  if (n == 1) {
+    Signal y = {n, calloc(n, sizeof(uint))};
+    y.signal[0] = s.signal[0];
+    return y;
+  }
+  uint x = 1;
+  Signal *ss = _splitEvenOdd(s);  // ss[0] = a_even, ss[1] = a_odd
+  ss[2] = _ntt(ss[0], (omega * omega) % PRIME);   // ss[2] = y_even
+  ss[3] = _ntt(ss[1], (omega * omega) % PRIME);   // ss[3] = y_odd
+  Signal y = {n, calloc(n, sizeof(uint))};
+  for (int i = 0; i < n/2; i++) {
+    y.signal[i]     = (ss[2].signal[i] + x * ss[3].signal[i]) % PRIME;
+    y.signal[i+n/2] = (ss[2].signal[i] + PRIME - x * ss[3].signal[i] % PRIME) % PRIME;
+    x = (x * omega) % PRIME;
+  }
+  freeSignals(ss, 4);
+  return y; 
+}
+
+// finds the omega needed to use for the ntt based on the length
+uint _omega(const int length) {
+  uint len = LEN;
+  uint omega = OMEGA;
+  while (len != length) {
+    len /= 2;
+    omega = (omega * omega) % PRIME;
+  }
+  return omega;
+}
+
+// slow but simple modular inverse of n mod PRIME
+uint _modInverse(const uint n) {
+  uint inverse = n;
+  while (inverse * n % PRIME != 1) {
+    inverse++;
+  }
+  return inverse;
+}
+
+Signal ntt(const Signal s) {
+  return _ntt(s, _omega(s.length));
+}
+
+Signal intt(const Signal s) {
+  Signal y =  _ntt(s, _modInverse(_omega(s.length)));
+  uint x = _modInverse(s.length);
+  for (int i = 0; i < s.length; i++) {
+    y.signal[i] = y.signal[i] * x % PRIME;
+  }
+  return y;
+}
