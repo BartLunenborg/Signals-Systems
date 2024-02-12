3458555bc5b544ec5a2097f4deeb3f880753a03d
Bart Lunenborg <b.lunenborg@student.rug.nl>
2024-02-12 12:31:17 +0100

    Small changes
    Changes to names and documentation/comments.

--- labThree/1/rust/src/signals.rs
+++ labThree/1/rust/src/signals.rs
@@ -41,24 +41,16 @@ pub fn read_signal() -> Signal {
 /// Prints a Signal to standard output.
 /// # Examples
 /// ```
-/// let s: Signal = Signal { 
-///     length: 5, 
-///     data: vec![
-///     Complex64::new(1.0, 1.0),
-///     Complex64::new(2.0, 2.0),
-///     Complex64::new(3.0, 3.0),
-///     Complex64::new(4.0, 4.0),
-///     Complex64::new(5.0, 5.0),
-///     ],
-/// };
+/// // s is a Signal with data [1+ni, 2+ni, 3+ni, 4+ni, 5+ni]
+/// // where n can be anything
 /// print_signal(&s);
 /// // Output: 5: [1,2,3,4,5]
 /// ```
 pub fn print_signal(s: &Signal) {
     let formatted_data: String = s.data
         .iter()
-        .map(|x| x.re.round() as i32)
-        .map(|x| x.to_string())
+        .map(|num| num.re.round() as i32)
+        .map(|num| num.to_string())
         .collect::<Vec<String>>()
         .join(",");
     println!("{}: [{}]", s.length, formatted_data);
@@ -95,8 +87,8 @@ fn fft(s: Signal, omega: Complex64) -> Signal {
     let un: usize = n.try_into().unwrap();
     let mut result: Vec<_> = vec![Complex64 {re: 0.0, im: 0.0}; un];
 
-    for i in 0..(un / 2) {
-        result[i] = even_fft.data[i] + x * odd_fft.data[i];
+    for i in 0..(un/2) {
+        result[i]      = even_fft.data[i] + x * odd_fft.data[i];
         result[i+un/2] = even_fft.data[i] - x * odd_fft.data[i];
         x *= omega;
     }
@@ -104,38 +96,35 @@ fn fft(s: Signal, omega: Complex64) -> Signal {
     Signal {length: n, data: result}
 }
 
-/// Creates a new Signal of length n.
-/// The new data will be padded with 0's to fill.
+/// Creates a new Signal of length n padded with 0's
 fn pad_signal(s: &Signal, n: i32) -> Signal {
     let mut padded_data = s.data.clone();
-    padded_data.resize_with(n.try_into().unwrap(), || num_complex::Complex::new(0.0, 0.0));
+    padded_data.resize_with(n.try_into().unwrap(), || Complex64 {re: 0.0, im: 0.0});
     Signal { length: n, data: padded_data }
 }
 
-/// Given two Signals, return the convolution calculated with the fft.
+/// Given two Signals, returns the convolution calculated using the fft.
 pub fn convolve(x: &Signal, y: &Signal) -> Signal {
-    let length = x.length + y.length - 1;
-    let n = power_of_two(length);
+    let z_length = x.length + y.length - 1;
+    let n = power_of_two(z_length);
 
-    // pad x and y with 0's
     let x_padded: Signal = pad_signal(x, n);
     let y_padded: Signal = pad_signal(y, n);
 
-    // fft of padded x and y
     let omega: Complex64 = Complex64::from_polar(1.0, 2.0 * PI / n as f64);
     let xfft = fft(x_padded, omega);
     let yfft = fft(y_padded, omega);
 
-    // component multiply
-    let xy: Vec<_> = xfft.data.iter()
+    let xy: Vec<_> = xfft.data
+        .iter()
         .zip(yfft.data.iter())
         .map(|(x, y)| x * y)
         .collect();
 
-    // inverse fft and adjust by / n
-    let result: Vec<_> = fft(Signal {length: n, data: xy}, omega.conj()).data.iter()
-        .map(|num| num / n as f64)
+    let z_vec: Vec<_> = fft(Signal {length: n, data: xy}, omega.conj()).data
+        .iter()
+        .map(|x| x / n as f64)
         .collect();
 
-    Signal {length, data: result[..length as usize].to_vec()}
+    Signal {length: z_length, data: z_vec[..z_length as usize].to_vec()}
 }
